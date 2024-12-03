509812fe88b60c526b03521f0a615c00e5a61032
sally Amedior <amediorsarah@gmail.com>
2024-12-03 17:13:35 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const header = document.querySelector("header");
+const navLinks = document.querySelectorAll(".nav-list a");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 10) {
+        header.classList.add("sticky");
+        navLinks.forEach(link => {
+            link.style.color = '#fff'; // Change nav link text color to white
+        });
+    } else {
+        header.classList.remove("sticky");
+        navLinks.forEach(link => {
+            link.style.color = 'var(--text-color)'; // Reset nav link text color to default
+        });
+    }
+});
+
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    navlist.classList.remove('open')
+}
+
+
+const scr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: 2500,
+    reset: true
+})
+
+scr.reveal('.home-text', {delay: 300});
+scr.reveal('.home-img', {delay: 400});
+scr.reveal('.container', {delay: 400});
+
+scr.reveal('.about-img', {});
+scr.reveal('.about-text', {delay: 300});
+
+scr.reveal('.middle-text', {});
+scr.reveal('.row-btn,.shop-content', {delay: 300});
+
+scr.reveal('.middle-text', {});
+scr.reveal('.review-content,.contact', {delay: 300});
